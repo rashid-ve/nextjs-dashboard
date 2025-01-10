522b3629535d6d5dc6a74b998f97989ecddd27de
rashid-ve <rashidashraf@teckvalley.com>
2025-01-10 19:21:54 +0530

    lint-fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -128,13 +128,15 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice" };
-  } catch (error) {
-    return {
-      message: "Database Error: Failed to delete Invoice.",
-    };
-  }
+  // try {
+  //   await sql`DELETE FROM invoices WHERE id = ${id}`;
+  //   revalidatePath("/dashboard/invoices");
+  //   return { message: "Deleted Invoice" };
+  // } catch (error) {
+  //   return {
+  //     message: "Database Error: Failed to delete Invoice.",
+  //   };
+  // }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
