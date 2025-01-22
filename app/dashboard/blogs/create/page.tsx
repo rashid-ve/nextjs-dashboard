99d797b3d4cf112eb3814d78392b9bef41965c73
rashid-ve <rashidashraf@teckvalley.com>
2025-01-22 19:18:47 +0530

    Dynamic Metadata for pages.

--- app/dashboard/blogs/create/page.tsx
+++ app/dashboard/blogs/create/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <div>Create Blog Page</div>;
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Blogs", href: "/dashboard/blogs" },
+          {
+            label: "Write New Blog",
+            href: "/dashboard/blogs/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
 }
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,10 +11,14 @@ import {
 import { Button } from "@/app/ui/button";
 import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
